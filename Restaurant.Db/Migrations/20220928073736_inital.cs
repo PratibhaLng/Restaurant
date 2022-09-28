3f7236eb978e1af0c0499aadf855462e0bc96db2
impratibha <pratibha.kaur@lngconsultancy.co.in>
2022-09-28 13:16:39 +0530

    update customer

---
+++ Restaurant.Db/Migrations/20220928073736_inital.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurant.Db.Migrations
+{
+    public partial class inital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactName = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    PhoneNo = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ Restaurant/Controllers/CustomerController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Db;
+using Restaurant.Model;
+using System;
+
+namespace Restaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+
+
+
+
+        private readonly ICustomerRepository _ccustomer;
+
+        public CustomerController(ICustomerRepository ccustomer)
+        {
+            _ccustomer = ccustomer;
+
+
+        }
+
+
+
+        [HttpGet]
+        [Route("api/[controller]")]
+
+
+        public IActionResult GetAllCustomer()
+        {
+            try
+            {
+
+                return Ok(_ccustomer.GetAllCustomer());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in Retreiving Data from Database");
+
+            }
+
+
+        }
+        [HttpPost]
+        [Route("api/[controller]")]
+
+        public IActionResult AddCustomer(Customer customer)
+        {
+            _ccustomer.AddCustomer(customer);
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Host + HttpContext.Request.Path + "/" + customer.Id, customer);
+
+
+
+
+        }
+
+
+
+
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetCustomer(int id)
+        {
+            try
+            {
+                var resultId = _ccustomer.GetCustomer(id);
+                if (resultId == null)
+                {
+                    return NotFound();
+                }
+                return Ok(resultId);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error in Retreiving Data from Database");
+
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("delete/Id")]
+        public IActionResult DeleteCustomer(int Id)
+
+
+        {
+            var removeCustomer = _ccustomer.GetCustomer(Id);
+            if (removeCustomer == null)
+            {
+
+
+
+                return NotFound($"Category With Id:{Id}  was not found");
+            }
+
+            {
+
+                _ccustomer.DeleteCustomer(Id);
+                return Ok(removeCustomer);
+
+            }
+        }
+
+
+
+
+
+        [HttpPut]
+        [Route("Edit/Id")]
+        public IActionResult EditCustomer(int id, Customer customer)
+
+        {
+            // var existingCustomer = _ccustomer.GetCustomer(id);
+            // if (existingCustomer != null)
+            //   customer.Id = existingCustomer.Id;
+            _ccustomer.EditCustomer(customer);
+            return Ok(customer);
+        }
+
+
+
+    }
+}
